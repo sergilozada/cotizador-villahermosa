d215df1e209e91f770931df3845ba29b6df87229
sergilozada <mythosw06@gmail.com>
2026-07-07 19:21:10 -0500

    agregar-usuario-nidia-mejia

--- src/script.js
+++ src/script.js
@@ -14,6 +14,11 @@ const users = {
     phone: '912131159',
     photo: '/src/assets/DIXON.jpeg',
   },
+  'n.mejia': {
+    advisor: 'Nidia Mejia',
+    fullName: 'Nidia Oriele Mejia Salcedo',
+    phone: '935106285',
+  },
   'l.villanueva': {
     advisor: 'Lizbeth Villanueva',
     fullName: 'Lizbeth Antonia Villanueva Chávez',
